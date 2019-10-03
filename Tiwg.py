f7f0e579d2649391ea2a6bf752f4c90651cc0231
Rafael Valenzuela <47725050+ravamo@users.noreply.github.com>
2019-10-03 15:06:01 +0200

    Add files via upload

--- Tiwg.py
+++ Tiwg.py
@@ -1,5 +1,4 @@
-import array as arr
-
+import math
 class twig:
 
     def check(listvalue, chunks):
@@ -12,32 +11,24 @@ class twig:
 
     def groupArrayElements(listValues, count):
         if twig.check(listValues ,count):
-            numElemens = round(len(listValues) / count)
-            return [listValues[i:i + 1 * numElemens] for i in range(0, len(listValues), numElemens)]
+            numElemens = math.ceil(len(listValues) / count)
+            list = [listValues[i:i + 1 * numElemens] for i in range(0, len(listValues), numElemens)]
+            # case when the num of list is < of element
+            if len(list) < count:
+                for i in range(0, count-len(list)):
+                    list.append([])
+            return list
         else:
             return None
-
-    def groupArrayElementsAlternative(data,chunk):
-        if twig.check(data, chunk):
-            numElemens = round(len(data) / chunk)
-            list=[]
-            while 0 < len(data):
-                if len(data) < numElemens:
-                    a = data[0:numElemens]
-                    del data[0:numElemens]
-                    list.append(a)
-                    break
-                else:
-                    a = data[0:numElemens]
-                    del data[0:numElemens]
-                    list.append(a)
-            return list
 
 
 if __name__ == '__main__':
     tiwg=twig()
     print(twig.check([1, 2, 3, 4, 5], 3))
-    print(twig.groupArrayElementsAlternative([1, 2, 3, 4, 5], 3))
+    print(twig.groupArrayElements([1, 2, 3, 4,5,6,7,8,9,10], 6))
+    print(twig.groupArrayElements([1, 2, 3, 4,5,6,7,8,9,10,11,12,13], 6))
+    print(twig.groupArrayElements([1,2,3,4,5,6,7], 6))
+
 
 
 
